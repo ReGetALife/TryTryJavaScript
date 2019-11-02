a88fdb4b36ad60d4ad002d5e78d2529c4c5af09a
GetALife <1340482017@qq.com>
2019-11-02 23:09:31 +0800

    :construction: updated chapter 2.1.1

---
+++ 2_在 HTML 中使用 JavaScript/inBody.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 放在 body 中的 js
+ * @Author: Li Qingguo
+ * @Date: 2019-11-02 23:03:08
+ * @FilePath: \2_在 HTML 中使用 JavaScript\inBody.js
+ * @Environment: Google Chrome 78.0.3904.87
+ * @LastEditor: Li Qingguo
+ * @LastEditedTime: 2019-11-02 23:03:36
+ */
+alert("after content");
---
+++ 2_在 HTML 中使用 JavaScript/inHead.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 在 head 中引入的 js
+ * @Author: Li Qingguo
+ * @Date: 2019-11-02 23:01:19
+ * @FilePath: \2_在 HTML 中使用 JavaScript\inHead.js
+ * @Environment: Google Chrome 78.0.3904.87
+ * @LastEditor: Li Qingguo
+ * @LastEditedTime: 2019-11-02 23:01:59
+ */
+alert("before content");
