15cb87f99051233d3644613524116490470e19bc
GetALife <1340482017@qq.com>
2019-11-02 23:29:44 +0800

    :construction: updated chapter 2.1.2

---
+++ 2_在 HTML 中使用 JavaScript/defer.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 延迟加载的 js
+ * @Author: Li Qingguo
+ * @Date: 2019-11-02 23:13:13
+ * @FilePath: \2_在 HTML 中使用 JavaScript\defer.js
+ * @Environment: Google Chrome 78.0.3904.87
+ * @LastEditor: Li Qingguo
+ * @LastEditedTime: 2019-11-02 23:13:45
+ */
+alert("defer js");
