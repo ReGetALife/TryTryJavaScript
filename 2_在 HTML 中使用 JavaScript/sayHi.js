81a4c1de028520acde8816c2330c4628e5ad36d3
GetALife <1340482017@qq.com>
2019-11-02 21:35:30 +0800

    :construction: updated chapter 2.1

---
+++ 2_在 HTML 中使用 JavaScript/sayHi.js
@@ -0,0 +1,10 @@
+/*
+ * @Description: 一个简单的外部 js
+ * @Author: Li Qingguo
+ * @Date: 2019-11-02 20:48:52
+ * @FilePath: \2_在 HTML 中使用 JavaScript\sayHi.js
+ * @Environment: Google Chrome 78.0.3904.87
+ * @LastEditor: Li Qingguo
+ * @LastEditedTime: 2019-11-02 21:24:40
+ */
+alert("outter js");
